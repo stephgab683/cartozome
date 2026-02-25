2ec256201a2fd70193c72a556ac97b49194c1f5c
Elias Dupont <elias.makhlouf@hotmail.fr>
2026-02-25 14:04:22 +0100

    Config

--- app/main.js
+++ app/main.js
@@ -2,16 +2,18 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
 
 const map = new Map({
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
+  layers: [ new TileLayer({ source: new OSM()})],
   view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+    center: fromLonLat([4.832391,45.757295]),
+    zoom: 15,
+  }),
 });
+
+
+
+
+
