7f3237b60694a06fe508d8e796297243dc21e5b9
LittleTrua <tanouk67@gmail.com>
2026-02-26 11:32:03 +0100

    police + bouton hamburger

--- app/main.js
+++ app/main.js
@@ -6,3 +6,11 @@ const map = L.map('map').setView([45.757295, 4.832391], 15);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
+
+// Bouton hamburger
+const hamburger = document.getElementById('hamburger');
+const sidePanel = document.getElementById('side-panel');
+
+hamburger.addEventListener('click', () => {
+  sidePanel.classList.toggle('collapsed');
+});
