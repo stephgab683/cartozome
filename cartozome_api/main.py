c1e0a0490f29a331c43c8fd818f2ded908f0131c
Elias Dupont <elias.makhlouf@hotmail.fr>
2026-03-12 00:20:28 +0100

    correction CORS

--- cartozome_api/main.py
+++ cartozome_api/main.py
@@ -13,6 +13,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost:5174",
         "http://localhost:5173",
         "http://localhost",
         "http://localhost:80",
